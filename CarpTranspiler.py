92776c5e8f1c7bcf03fd53d17fe11252e00606df
Dibbo <DeflatedPickle@gmail.com>
2018-01-26 21:36:02 +0000

    Updated transpiler

--- CarpTranspiler.py
+++ CarpTranspiler.py
@@ -8,19 +8,29 @@ from CarpLexer import CarpLexer
 from CarpListener import CarpListener
 from CarpParser import CarpParser
 
+import io
 
-class CarpInterpreter(CarpListener):
-    def __init__(self):
-        pass
+
+class CarpTranspiler(CarpListener):
+    def __init__(self, output: io.FileIO):
+        self.output = output
+
+        self.context = None
+
+    def enterNormalClass(self, ctx:CarpParser.NormalClassContext):
+        self.output.write(f"class {str(ctx.ID())}%s" % "{")
+
+    def exitNormalClass(self, ctx:CarpParser.NormalClassContext):
+        self.output.write("}")
 
 
 if __name__ == "__main__":
-    lexer = CarpLexer(antlr4.FileStream("example.carp"))
-    stream = antlr4.CommonTokenStream(lexer)
+    lexerer = CarpLexer(antlr4.FileStream("example.carp"))
+    stream = antlr4.CommonTokenStream(lexerer)
     parser = CarpParser(stream)
     tree = parser.program()
 
-    interpret = CarpInterpreter()
-    walker = antlr4.ParseTreeWalker()
-    walker.walk(interpret, tree)
-
+    with open("example.cs", "w") as out:
+        interpreter = CarpTranspiler(out)
+        walker = antlr4.ParseTreeWalker()
+        walker.walk(interpreter, tree)
